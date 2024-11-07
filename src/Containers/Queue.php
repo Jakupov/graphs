867ac55a6a9eb8832e494925cc166b45da762744
Ilyas Jakupov <25123645+Jakupov@users.noreply.github.com>
2024-11-07 12:19:11 +0500

    Queue refactoring
    Makes dequeue to work `O(1)` instead of `O(n)`.

--- src/Containers/Queue.php
+++ src/Containers/Queue.php
@@ -9,16 +9,36 @@ class Queue
      */
     private array $queue = [];
 
+    /**
+     * Adds given element to the end of queue
+     *
+     * @param mixed $data Element to be added to queue
+     *
+     * @return void
+     */
     public function enqueue(mixed $data): void
     {
         $this->queue[] = $data;
     }
 
+    /**
+     * Returns first element from queue and removes it
+     *
+     * @return false|mixed Returns false if queue is empty, otherwise returns first element in queue
+     */
     public function dequeue(): mixed
     {
-        return array_shift($this->queue);
+        $a = current($this->queue);
+        unset($this->queue[key($this->queue)]);
+
+        return $a;
     }
 
+    /**
+     * Checks if queue is empty
+     *
+     * @return bool
+     */
     public function isEmpty(): bool
     {
         return empty($this->queue);
